9b035adf3145f6bb53427c696a758d8050f8e698
eevi-ln <nlambert@usm.cl>
2026-07-02 22:56:20 -0400

    added solution with trimming

--- code/backtracking-sol.cpp
+++ code/backtracking-sol.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
 #include <vector>
 
-void backtracking(const int& n, const std::vector<int>& original, std::vector<bool>& disponible, std::vector<int>& actual, int threshold, int& maximo)
+void backtracking(const int& n, const std::vector<int>& original, std::vector<bool>& disponible, std::vector<int>& actual, int threshold, int& maximo, int& superiores)
 {
-	if(actual.size() == n)
-	{
-		int superior = 0;
-		for(int i = 0; i < n; i++)
-		{
-			if(actual[i] > original[i]) superior++;
-		}
-		maximo = (superior > maximo ? superior : maximo);
-	}
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < n && n - actual.size() >= maximo - superiores; i++)
 	{
 		if(disponible[i])
 		{
 			actual.push_back(original[i]);
+			if(original[i] > original[actual.size()-1]) superiores++;
 			disponible[i] = false;
-			backtracking(n, original, disponible, actual, i+1, maximo);
+			backtracking(n, original, disponible, actual, i+1, maximo, superiores);
 			disponible[i] = true;
+			if(original[i] > original[actual.size()-1]) superiores--;
 			actual.pop_back();
 		}
 	}
+	maximo = superiores > maximo ? superiores : maximo;
 }
 
 int solucion(const int& n, const std::vector<int>& numeros)
@@ -30,7 +24,8 @@ int solucion(const int& n, const std::vector<int>& numeros)
 	std::vector<bool> disponible(n, true);
 	std::vector<int> actual;
 	int maximo = 0;
-	backtracking(n, numeros, disponible, actual, 0, maximo);
+	int superiores = 0;
+	backtracking(n, numeros, disponible, actual, 0, maximo, superiores);
 
 	return maximo;
 }
